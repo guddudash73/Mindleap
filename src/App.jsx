e7215e84b89a9568bcd7c5679b7ca52c6a5fe2b9
Rashmiranjan_Dash <guddudash73@gmail.com>
2025-07-22 04:29:45 +0530

    Added the Nav bar and some scrolling Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">Hello World</h1>
-    </>
+    <div className="font-primary overflow-x-hidden">
+      <Navbar></Navbar>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,189 @@
+import React, { useEffect, useState } from "react";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { motion } from "motion/react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState("home");
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
+  const handleScroll = () => {
+    const sections = ["home", "services", "about", "pricing", "testimonial"];
+    const scrollPosition = window.scrollY + 100;
+
+    sections.forEach((sections) => {
+      //understand this logic
+      const element = document.getElementById(sections);
+      if (element) {
+        const offsetTop = element.offsetTop;
+        const height = element.offsetHeight;
+        if (
+          scrollPosition >= offsetTop &&
+          scrollPosition < offsetTop + height
+        ) {
+          setActiveSection(sections);
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScrollTo = (targetId) => {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({ top: targetElement.offsetTop, behavior: "smooth" });
+    }
+  };
+
+  const navLinks = (
+    <ul className="font-medium flex flex-col md:flex-row lg:space-x-8 sm:space-x-4 space-y-2 md:space-y-0 p-4 md:p-0">
+      <li>
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          href="#home"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseMenu();
+            handleScrollTo("home");
+          }}
+          className={`text-white ${activeSection === "home" ? "isActive" : ""}`}
+        >
+          Home
+        </motion.a>
+      </li>
+      <li>
+        <a
+          href="#services"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseMenu();
+          }}
+          className={`text-white ${
+            activeSection === "services" ? "isActive" : ""
+          }`}
+        >
+          Services
+        </a>
+      </li>
+      <li>
+        <a
+          href="#about"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseMenu();
+          }}
+          className={`text-white ${
+            activeSection === "about" ? "isActive" : ""
+          }`}
+        >
+          About Us
+        </a>
+      </li>
+      <li>
+        <a
+          href="#pricing"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseMenu();
+          }}
+          className={`text-white ${
+            activeSection === "pricing" ? "isActive" : ""
+          }`}
+        >
+          Pricing
+        </a>
+      </li>
+      <li>
+        <a
+          href="#testimonial"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCloseMenu();
+          }}
+          className={`text-white ${
+            activeSection === "testimonial" ? "isActive" : ""
+          }`}
+        >
+          Testimonial
+        </a>
+      </li>
+    </ul>
+  );
+  return (
+    <header className="bg-heroBg text-white py-6 px-4 fixed top-0 left-0 right-0 z-10">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        {/* logo */}
+        <div>
+          <a href="/"></a>
+          <img src="/logo.svg" alt="Mind Leap Logo" />
+        </div>
+
+        {/* navItems */}
+
+        <div className="hidden md:flex flex-grow justify-center">
+          <nav>{navLinks}</nav>
+        </div>
+
+        {/* button */}
+
+        <div className="hidden md:block">
+          <a
+            href="#contact"
+            className="text-white bg-primary hover:bg-primary/90 px-4 py-2 rounded"
+          >
+            Contact Us
+          </a>
+        </div>
+
+        {/* hamburger menu */}
+
+        <div className="block md:hidden">
+          <button
+            onClick={handleToggle}
+            className={`text-white focus:outline-none ${
+              isOpen ? "border border-white" : ""
+            }`}
+          >
+            <HiOutlineMenuAlt3 className="size-6"></HiOutlineMenuAlt3>
+          </button>
+        </div>
+      </div>
+
+      {/* mobile nav items */}
+
+      {isOpen && (
+        <nav className="absolute top-full left-0 w-full bg-heroBg z-20 md:hidden">
+          <ul className="flex flex-col p-4 space-y-3 ">
+            {navLinks.props.children}
+            <li className="py-2 list-none">
+              <a
+                href="#contact"
+                className="text-white bg-primary hover:bg-primary/90 px-4 py-2 rounded "
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleCloseMenu();
+                }}
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
