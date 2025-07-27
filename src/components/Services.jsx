bc96119c7fed280128f005246cc89804975cca77
Rashmiranjan_Dash <guddudash73@gmail.com>
2025-07-28 00:01:26 +0530

    fixed services card animation

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -45,119 +45,151 @@ const Services = () => {
               <Tab>Self-Confidence</Tab>
             </motion.TabList>
 
-            <TabPanel className="flex flex-col justify-center items-center lg:flex-row gap-8">
-              <div className=" md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
-                <h3 className="text-3xl font-semibold text-primary mb-4">
-                  Couple Counseling
-                </h3>
-                <p className="mb-8">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Iusto, a. Incidunt animi mollitia alias, eum eligendi in? Id
-                  veritatis quos hic, provident error maiores blanditiis?
-                </p>
+            <TabPanel className="w-auto">
+              <motion.div
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="flex flex-col justify-center items-center lg:flex-row gap-8"
+              >
+                <div className=" md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
+                  <h3 className="text-3xl font-semibold text-primary mb-4">
+                    Couple Counseling
+                  </h3>
+                  <p className="mb-8">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Iusto, a. Incidunt animi mollitia alias, eum eligendi in? Id
+                    veritatis quos hic, provident error maiores blanditiis?
+                  </p>
 
-                <h4 className="text-xl font-medium text-black mb-4">
-                  Benefits
-                </h4>
-                <ul className="list-disc list-inside space-y-3">
-                  <li>Understanding Relationship Dynamics</li>
-                  <li>Understanding Relationship Dynamics</li>
-                  <li>Understanding Relationship Dynamics</li>
-                </ul>
-              </div>
-              <div className="md:w-2/3 lg:w-1/2">
-                <img
-                  src={serviceImg1}
-                  alt="Couple Counseling Img"
-                  className="w-full h-auto rounded-2xl object-cover"
-                />
-              </div>
+                  <h4 className="text-xl font-medium text-black mb-4">
+                    Benefits
+                  </h4>
+                  <ul className="list-disc list-inside space-y-3">
+                    <li>Understanding Relationship Dynamics</li>
+                    <li>Understanding Relationship Dynamics</li>
+                    <li>Understanding Relationship Dynamics</li>
+                  </ul>
+                </div>
+                <div className="md:w-2/3 lg:w-1/2">
+                  <img
+                    src={serviceImg1}
+                    alt="Couple Counseling Img"
+                    className="w-full h-auto rounded-2xl object-cover"
+                  />
+                </div>
+              </motion.div>
             </TabPanel>
-            <TabPanel className="flex flex-col justify-center items-center lg:flex-row gap-8">
-              <div className=" md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
-                <h3 className="text-3xl font-semibold text-primary mb-4">
-                  Parenting Skills
-                </h3>
-                <p className="mb-8">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                  nam, sit, autem quas temporibus ad dolorum et sequi doloribus
-                  veritatis placeat officia tempora natus possimus quisquam
-                  deleniti?
-                </p>
+            <TabPanel className="w-auto">
+              <motion.div
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="flex flex-col justify-center items-center lg:flex-row gap-8"
+              >
+                <div className=" md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
+                  <h3 className="text-3xl font-semibold text-primary mb-4">
+                    Parenting Skills
+                  </h3>
+                  <p className="mb-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Neque nam, sit, autem quas temporibus ad dolorum et sequi
+                    doloribus veritatis placeat officia tempora natus possimus
+                    quisquam deleniti?
+                  </p>
 
-                <h4 className="text-xl font-medium text-black mb-4">
-                  Benefits
-                </h4>
-                <ul className="list-disc list-inside space-y-3">
-                  <li>Lorem, ipsum dolor.</li>
-                  <li>Lorem, ipsum dolor.</li>
-                  <li>Lorem ipsum dolor sit.</li>
-                </ul>
-              </div>
-              <div className="md:w-2/3 lg:w-1/2">
-                <img
-                  src={serviceImg2}
-                  alt="Parenting Skills Img"
-                  className="w-full h-auto rounded-2xl object-cover"
-                />
-              </div>
+                  <h4 className="text-xl font-medium text-black mb-4">
+                    Benefits
+                  </h4>
+                  <ul className="list-disc list-inside space-y-3">
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                  </ul>
+                </div>
+                <div className="md:w-2/3 lg:w-1/2">
+                  <img
+                    src={serviceImg2}
+                    alt="Parenting Skills Img"
+                    className="w-full h-auto rounded-2xl object-cover"
+                  />
+                </div>
+              </motion.div>
             </TabPanel>
-            <TabPanel className="flex flex-col justify-center items-center lg:flex-row gap-8">
-              <div className="md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
-                <h3 className="text-3xl font-semibold text-primary mb-4">
-                  feeling Stuck
-                </h3>
-                <p className="mb-8">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Officiis exercitationem doloribus doloremque rem cum
-                  consequatur laborum sit in fugit? Consequatur.
-                </p>
+            <TabPanel className="w-auto">
+              <motion.div
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="flex flex-col justify-center items-center lg:flex-row gap-8"
+              >
+                <div className="md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
+                  <h3 className="text-3xl font-semibold text-primary mb-4">
+                    feeling Stuck
+                  </h3>
+                  <p className="mb-8">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Officiis exercitationem doloribus doloremque rem cum
+                    consequatur laborum sit in fugit? Consequatur.
+                  </p>
 
-                <h4 className="text-xl font-medium text-black mb-4">
-                  Benefits
-                </h4>
-                <ul className="list-disc list-inside space-y-3">
-                  <li>Lorem ipsum dolor sit amet.</li>
-                  <li>Lorem, ipsum dolor.</li>
-                  <li>Lorem ipsum dolor sit amet consectetur.</li>
-                </ul>
-              </div>
-              <div className="md:w-2/3 lg:w-1/2">
-                <img
-                  src={serviceImg3}
-                  alt="Felling Stuck Img"
-                  className="w-full h-auto rounded-2xl object-cover"
-                />
-              </div>
+                  <h4 className="text-xl font-medium text-black mb-4">
+                    Benefits
+                  </h4>
+                  <ul className="list-disc list-inside space-y-3">
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                  </ul>
+                </div>
+                <div className="md:w-2/3 lg:w-1/2">
+                  <img
+                    src={serviceImg3}
+                    alt="Felling Stuck Img"
+                    className="w-full h-auto rounded-2xl object-cover"
+                  />
+                </div>
+              </motion.div>
             </TabPanel>
-            <TabPanel className="flex flex-col justify-center items-center lg:flex-row gap-8">
-              <div className="md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
-                <h3 className="text-3xl font-semibold text-primary mb-4">
-                  Self-Confident
-                </h3>
-                <p className="mb-8">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
-                  eligendi vel explicabo rerum sunt accusamus consequatur
-                  tenetur asperiores obcaecati id sed ea, nobis fuga veritatis
-                  similique. Odio quas id temporibus!
-                </p>
+            <TabPanel className="w-auto">
+              <motion.div
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="flex flex-col justify-center items-center lg:flex-row gap-8"
+              >
+                <div className="md:w-2/3 lg:w-1/2 bg-white rounded-lg p-12 font-secondary">
+                  <h3 className="text-3xl font-semibold text-primary mb-4">
+                    Self-Confident
+                  </h3>
+                  <p className="mb-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+                    eligendi vel explicabo rerum sunt accusamus consequatur
+                    tenetur asperiores obcaecati id sed ea, nobis fuga veritatis
+                    similique. Odio quas id temporibus!
+                  </p>
 
-                <h4 className="text-xl font-medium text-black mb-4">
-                  Benefits
-                </h4>
-                <ul className="list-disc list-inside space-y-3">
-                  <li>Lorem ipsum dolor sit amet consectetur.</li>
-                  <li>Lorem ipsum dolor sit.</li>
-                  <li>Lorem ipsum dolor sit amet.</li>
-                </ul>
-              </div>
-              <div className="md:w-2/3 lg:w-1/2 ">
-                <img
-                  src={serviceImg4}
-                  alt="Slef-Confident Img"
-                  className="w-full h-auto rounded-2xl object-cover"
-                />
-              </div>
+                  <h4 className="text-xl font-medium text-black mb-4">
+                    Benefits
+                  </h4>
+                  <ul className="list-disc list-inside space-y-3">
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    <li>Lorem ipsum dolor sit.</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                  </ul>
+                </div>
+                <div className="md:w-2/3 lg:w-1/2 ">
+                  <img
+                    src={serviceImg4}
+                    alt="Slef-Confident Img"
+                    className="w-full h-auto rounded-2xl object-cover"
+                  />
+                </div>
+              </motion.div>
             </TabPanel>
           </Tabs>
         </div>
